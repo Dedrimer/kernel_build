5fe4740cadfd6dee30dfbc2af04e7b0fb1e73bfd
zclkkk <76833317+zclkkk@users.noreply.github.com>
2021-08-28 00:00:34 +0800

    FUCK U

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-CLANG_DIR=/home/runner/work/clang
+export PATH=clang/bin:${PATH}
 export KBUILD_BUILD_USER="zclkkk"
 export KBUILD_BUILD_HOST="Server"
-export PATH=/home/runner/work/clang/bin:${PATH}
 args="                  ARCH=arm64 \
                         O=../out \
                         CC=clang \
